e545baff9907f9d0e0913277268002f7b38ae6d8
xRen1101 <zairenas@gmail.com>
2017-09-08 19:36:13 +0300

    initial .net core mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhoneShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
